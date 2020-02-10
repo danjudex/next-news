fee0b90ebc61e16d516e1d612a4f90be5a0b80c6
danjudex <dan.judex@gmail.com>
2020-02-10 23:18:57 +0500

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '~/assets/css/album.css';
+
+export default function BlogApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,53 @@
+import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
+import AlbumCard from '~/components/AlbumCard';
+
+const HomePage = (): React.ReactNode => (
+  <main role="main">
+    <Jumbotron className="text-center">
+      <Container>
+        <h1 className="jumbotron-heading">Album example</h1>
+        <p className="lead text-muted">
+          Something short and leading about the collection below—its contents,
+          the creator, etc. Make it short and sweet, but not too short so folks
+          don't simply skip over it entirely.
+        </p>
+        <p>
+          <a href="#" className="btn btn-primary my-2">
+            Main call to action
+          </a>
+
+          <a href="#" className="btn btn-secondary my-2">
+            Secondary action
+          </a>
+        </p>
+      </Container>
+    </Jumbotron>
+
+    <div className="album py-5 bg-light">
+      <Container>
+        <Row>
+          <Col md="4">
+            <AlbumCard />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+    <footer className="text-muted">
+      <Container>
+        <p className="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>
+          Album example is © Bootstrap, but please download and customize it for
+          yourself!
+        </p>
+        <p>
+          New to Bootstrap? <a href="../../">Visit the homepage</a> or read our{' '}
+          <a href="../../getting-started/">getting started guide</a>.
+        </p>
+      </Container>
+    </footer>
+  </main>
+);
+
+export default HomePage;
